184af69805febd96dbb8890b599f5023517e5520
Anya <ania.leonova@gmail.com>
2020-01-11 19:10:39 -0800

    Display webcam video in the browser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// not using App.js for now, doing everything through index
+//import App from './App';
+import Webcam from 'react-webcam';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* 
+* DISPLAYING VIDEO FROM THE WEBCAM
+*/
+class WebcamComponent extends React.Component {
+    render() {
+        // specify the constraints for the webcam video
+        const videoConstraints = {
+            width: window.innerWidth,
+            height: window.innerHeight - 50,
+            facingMode: "user"
+            };
+
+        // render the webcam video with specific parameters
+        return <Webcam 
+            audio={false}
+            videoConstraints={videoConstraints}/>;
+    }
+  }
+
+// actually display stuff in the browser
+ReactDOM.render(<WebcamComponent />, document.getElementById('root'));
+
+// smth random???? -->
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
